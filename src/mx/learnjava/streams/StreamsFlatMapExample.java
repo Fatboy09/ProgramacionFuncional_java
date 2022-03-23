2139c77be871ccab16ad4ad45fdf098ef5271d15
Leiruz <jonysuri09@gmail.com>
2022-03-22 21:57:58 -0600

    uso de las operaciones distinct, sorted y count de un stream

--- src/mx/learnjava/streams/StreamsFlatMapExample.java
+++ src/mx/learnjava/streams/StreamsFlatMapExample.java
@@ -10,16 +10,29 @@ public class StreamsFlatMapExample {
 	
 	public static List<String> printActivities() {
 		List<String> studentActivities = StudentDataBase.getAllStudents().stream() // Stream<Student>
-			.map(Student::getActivities) // Stream<List<String>>
-			.flatMap(List::stream) //Stream<String>
+			.map(Student::getActivities)	// Stream<List<String>>
+			.flatMap(List::stream) 			// Stream<String>
+			.distinct()						// retorna un stream sin repeticiones
+			.sorted()						// ordena los elementos del stream
 			.collect(Collectors.toList());
 		
 		return studentActivities;
 	}
 	
+	public static long getActivitiesCount() {
+		long count = StudentDataBase.getAllStudents().stream() // Stream<Student>
+			.map(Student::getActivities) 	// Stream<List<String>>
+			.flatMap(List::stream)			//Stream<String>
+			.distinct()						// retorna un stream sin repeticiones
+			.count();						// retorna el total del elementos en el stream
+		
+		return count;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(printActivities());
+		System.out.println("Total Activities: " + getActivitiesCount());
 	}
 
 }
